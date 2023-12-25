e8ebcb25a4f0bb320f8da54a930a9a3ba603782d
Ravisankar Sivaji <ravi.sankar@aquera.com>
2023-12-25 14:11:59 +0530

    init

---
+++ src/menu-items/dashboard.tsx
@@ -0,0 +1,61 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
+// project-imports
+import { useSelector } from 'store';
+
+// type
+import { NavItemType } from 'types/menu';
+import { Home3, HomeTrendUp, Box1 } from 'iconsax-react';
+
+const icons = {
+  navigation: Home3,
+  dashboard: HomeTrendUp,
+  components: Box1
+};
+
+// ==============================|| MENU ITEMS - API ||============================== //
+
+export const Menu = () => {
+  const { menu } = useSelector((state) => state.menu);
+
+  const SubChildrenLis = (SubChildrenLis: NavItemType[]) => {
+    return SubChildrenLis?.map((subList: NavItemType) => {
+      return {
+        ...subList,
+        title: <FormattedMessage id={`${subList.title}`} />,
+        // @ts-ignore
+        icon: icons[subList.icon]
+      };
+    });
+  };
+
+  const itemList = (subList: NavItemType) => {
+    let list: NavItemType = {
+      ...subList,
+      title: <FormattedMessage id={`${subList.title}`} />,
+      // @ts-ignore
+      icon: icons[subList.icon]
+    };
+
+    if (subList.type === 'collapse') {
+      list.children = SubChildrenLis(subList.children!);
+    }
+    return list;
+  };
+
+  const withoutMenu = menu?.children?.filter((item: NavItemType) => item.id !== 'no-menu');
+  const ChildrenList: NavItemType[] | undefined = withoutMenu?.map((subList: NavItemType) => {
+    return itemList(subList);
+  });
+
+  const menuList: NavItemType = {
+    ...menu,
+    title: <FormattedMessage id={`${menu.title}`} />,
+    // @ts-ignore
+    icon: icons[menu.icon],
+    children: ChildrenList
+  };
+
+  return menuList;
+};
---
+++ src/sections/apps/calendar/ColorPalette.tsx
@@ -0,0 +1,67 @@
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Box, FormControlLabel, Radio, Tooltip } from '@mui/material';
+
+// project-imports
+import Avatar from 'components/@extended/Avatar';
+
+// assets
+import { TickSquare } from 'iconsax-react';
+
+// types
+import { ThemeMode } from 'types/config';
+
+// ==============================|| CALENDAR - COLOR PALETTE ||============================== //
+
+interface Props {
+  color: string;
+  value: string;
+  isLight?: boolean;
+}
+
+const ColorPalette = ({ color, value, isLight }: Props) => {
+  const theme = useTheme();
+
+  return (
+    <Tooltip title={color}>
+      <FormControlLabel
+        value={value}
+        label=""
+        control={
+          <Radio
+            icon={
+              <Avatar variant="rounded" type="combined" size="xs" sx={{ backgroundColor: color, borderColor: 'divider' }}>
+                <Box sx={{ display: 'none' }} />
+              </Avatar>
+            }
+            checkedIcon={
+              <Avatar
+                variant="rounded"
+                type="combined"
+                size="xs"
+                sx={{
+                  backgroundColor: color,
+                  color: isLight ? 'secondary.dark' : 'secondary.lighter',
+                  ...(theme.palette.mode === ThemeMode.DARK && {
+                    color: 'secondary.lighter'
+                  }),
+                  borderColor: 'divider',
+                  '& svg': { width: 20, height: 20 }
+                }}
+              >
+                <TickSquare variant="Bold" />
+              </Avatar>
+            }
+            sx={{
+              '&:hover': {
+                bgcolor: 'transparent'
+              }
+            }}
+          />
+        }
+      />
+    </Tooltip>
+  );
+};
+
+export default ColorPalette;
